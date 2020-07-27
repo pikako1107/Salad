e3e5e083bad3f31bb3ef553e2fb05a9813d8e819
光紗 佐藤 <m-satochu@outlook.jp>
2020-07-27 15:34:00 +0900

    ファイルパス変更

--- SaladBowl/SaladBowl/settings.py
+++ SaladBowl/SaladBowl/settings.py
@@ -121,5 +121,5 @@ LOGIN_REDIRECT_URL = 'index' # ログイン後のリダイレクト先
 LOGOUT_REDIRECT_URL = 'index' # ログアウト後のリダイレクト先
 
 # ファイル保存
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'import')
+MEDIA_URL = '/import/'
